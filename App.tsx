c7b15cad32af391bb6a0f5dbc793f2d6618141f8
hvrkarthik <hvrkarthik@gmail.com>
2023-03-23 10:23:59 +0530

    TypeScript

---
+++ App.tsx
@@ -0,0 +1,18 @@
+// import {View, Text} from 'react-native';
+// import React from 'react';
+// import AppNavigator from './src/AppNavigator';
+
+// const App = () => {
+//   return <AppNavigator />;
+// };
+
+// export default App;
+import { View, Text } from "react-native";
+import React from "react";
+import AppNavigator from "./src/AppNavigator";
+
+const App = () => {
+  return <AppNavigator />;
+};
+
+export default App;
