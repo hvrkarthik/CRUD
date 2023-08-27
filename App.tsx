f619edebf07e7dba0a45a89dff9eb9a4f8fbe9e4
Karthik N G <79392613+hvrkarthik@users.noreply.github.com>
2023-08-27 19:03:07 +0530

    issues Resolved

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import AppNavigator from './src/appnavigator';
+import AppNavigator from './src/AppNavigator';
 
 const App = () => {
   return <AppNavigator />;
--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -2,9 +2,9 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import Home from './screens/home';
-import Adduser from './screens/adduser';
-import Edituser from './screens/edituser';
+import Home from './screens/Home';
+import Adduser from './screens/AddUser';
+import Edituser from './screens/EditUser';
 const Stack = createStackNavigator();
 const AppNavigator = () => {
   return (
